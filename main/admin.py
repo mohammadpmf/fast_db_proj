6e7fcfa78dac799e61ac34c7bcf93dd1906952e3
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-11-24 13:18:02 +0330

    add debug toolbar and optimize showing products and categories

--- main/admin.py
+++ main/admin.py
@@ -9,7 +9,25 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'parent']
     list_display_links = ['id', 'title', 'description', 'parent']
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('parent').prefetch_related(
+            'parent__parent',  # 2nd layer
+            'parent__parent__parent',  # 3rd layer
+            'parent__parent__parent__parent',  # 4th layer, if necessary
+            'parent__parent__parent__parent__parent',  # 5th layer, if necessary
+        )
+    
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'category' ,'title' ,'description' ,'price' ,'extra_info']
     list_display_links = ['id', 'category' ,'title' ,'description' ,'price' ,'extra_info']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('category').prefetch_related(
+            'category__parent',  # 2nd layer
+            'category__parent__parent',  # 3rd layer
+            'category__parent__parent__parent',  # 4th layer, if necessary
+            'category__parent__parent__parent__parent',  # 5th layer, if necessary
+        )

b1e52b647385670c081ad8aaff83150c1033c5c8
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2025-01-07 21:00:47 +0330

    change viewsets to use replica

--- main/views/category.py
+++ main/views/category.py
@@ -1,21 +1,53 @@
 from rest_framework import viewsets, permissions
 
 from main.models.category import Category
-from main.serializers.category_serializer import CategoryReadSerializer, CategoryWriteSerializer
+from main.serializers.category_serializer import (
+    CategoryReadSerializer,
+    CategoryWriteSerializer,
+)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all().select_related('parent').prefetch_related(
-        'parent__parent',
-        'parent__parent__parent',
-        'parent__parent__parent__parent',
-        'parent__parent__parent__parent__parent',
-        'parent__parent__parent__parent__parent__parent',
-    )
-    serializer_class = CategoryReadSerializer
     permission_classes = [permissions.AllowAny]
 
+    def get_queryset(self):
+        """
+        Use replica for reads (GET) and master for writes (other methods).
+        """
+        db = "replica" if self.request.method in ["GET"] else "default"
+        return (
+            Category.objects.using(db)
+            .all()
+            .select_related("parent")
+            .prefetch_related(
+                "parent__parent",
+                "parent__parent__parent",
+                "parent__parent__parent__parent",
+                "parent__parent__parent__parent__parent",
+                "parent__parent__parent__parent__parent__parent",
+            )
+        )
+
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ["list", "retrieve"]:
             return CategoryReadSerializer
         return CategoryWriteSerializer
+
+    # # Inside your viewset methods if needed (optional override)
+    # def perform_create(self, serializer):
+    #     """
+    #     Save the object using the default (master) database.
+    #     """
+    #     serializer.save(using='default')
+
+    # def perform_update(self, serializer):
+    #     """
+    #     Update the object using the default (master) database.
+    #     """
+    #     serializer.save(using='default')
+
+    # def perform_destroy(self, instance):
+    #     """
+    #     Delete the object using the default (master) database.
+    #     """
+    #     instance.delete(using='default')
--- main/views/product.py
+++ main/views/product.py
@@ -1,19 +1,34 @@
-from rest_framework import permissions
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from main.models.product import Product
-from main.serializers.product_serializer import ProductReadSerializer, ProductWriteSerializer
+from main.serializers.product_serializer import (
+    ProductReadSerializer,
+    ProductWriteSerializer,
+)
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().select_related('category').prefetch_related(
-        'category__parent',
-        'category__parent__parent',
-        'category__parent__parent__parent',
-        'category__parent__parent__parent__parent',
-        )
     permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        Use replica for reads (GET) and master for writes (other methods).
+        """
+        db = "replica" if self.request.method in ["GET"] else "default"
+        return (
+            Product.objects.using(db)
+            .all()
+            .select_related("category")
+            .prefetch_related(
+                "category__parent",
+                "category__parent__parent",
+                "category__parent__parent__parent",
+                "category__parent__parent__parent__parent",
+            )
+        )
+
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        print(self.action)
+        if self.action in ["list", "retrieve"]:
             return ProductReadSerializer
         return ProductWriteSerializer
